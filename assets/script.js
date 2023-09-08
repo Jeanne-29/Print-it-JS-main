d909cf5aecf6ffdc121dfad54591501baa25d493
Jeanne <jeannecoudert4gmail.com>
2023-09-08 17:43:50 +0200

    Avant derniere etape

--- assets/script.js
+++ assets/script.js
@@ -26,37 +26,45 @@ const arrowRight = document.querySelector('.arrow_right')
 const arrowLeft = document.querySelector('.arrow_left')
 
 
-//Etape n°2 Ajoutez un écouteurs d'evenements 
 
-
-// Vérifiez s'ils existent avant d'ajouter des event listeners
-if (arrowLeft && arrowRight) {
-  arrowLeft.addEventListener("click", () => {
-    console.log("Flèche de gauche");
-    updateSlide(-1);
-  });
-
-  arrowRight.addEventListener("click", () => {
-    console.log("Flèche de droite");
-    updateSlide(1);
-  });
-} else {
-  console.error("Les éléments avec les classes 'arrow_left' et 'arrow_right' n'ont pas été trouvés dans le DOM.");
-}
-
-function updateSlide(direction) {
-  // Votre logique pour mettre à jour le diaporama avec la direction donnée
-}
-
-
-//Etape n°3 Ajoutez des bullet points au slider
+// Ajoutez des bullet points au slider
 
 slides.forEach((slide) => {
-	if (slides.length > 1 ) {
+	if (slides.length > 1 ) { //La dimension du slide est superieur a 1 (4)
 		const div = document.createElement('div');
 		div.className = 'dot';
 		dots.appendChild(div);
 	}
 })
 
+const dot = document.querySelectorAll('.dot')
+
+dot[0].classList.add('dot_selected')
+
+let currentSlide = 0
+
+function slide() {
+	bannerImg.src = './assets/images/slideshow/' + slides[currentSlide].image
+	bannerText.innerHTML = slides [currentSlide].tagLine
+	
+	dot[currentSlide].classList.add('dot_selected')
+}
+
+//Ajoutez un écouteurs d'evenements 
+
+arrowLeft.addEventListener('click',() => {
+	// Retire la classe 'dot_selected' de l'élément dot actuel
+	dot[currentSlide].classList.remove("dot_selected")
+
+// Vérifie si la diapositive actuelle est la première
+	if (currentSlide === 0) {
+		// Si c'est le cas, passe à la dernière diapositive
+		currentSlide = slides.lengt -1
+	} else {
+		// Sinon, passe à la diapositive précédente
+		currentSlide --
+	}
+	// Appelle la fonction slide() pour mettre à jour le diaporama
+	slide()
+})
 
